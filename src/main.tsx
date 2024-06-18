1b7e84cae2e81b07976719dbef1d15a59a405bef
Sebastian Grüßinger <sebastiangruessinger@gmail.com>
2024-06-18 23:36:35 +0200

    chore: setup component and table library

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const T_5_MINUTES = 1000 * 60 * 5;
@@ -20,7 +22,9 @@ export const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <App />
+      </MantineProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
